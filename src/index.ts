f994bd485d030129e366864252e3705d2f1d4afb
GeekEvan <352104389@qq.com>
2022-05-13 14:13:10 +0800

    feat:complete fun

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,56 @@
-let say: string = "hello"
-console.log(say)
+type Deck = NormalCard[]
+type Color = "♠" | "♥" | "♦" | "♣"
+type NormalCard = {
+  color: Color,
+  mark: number
+}
+
+function createDeck(): Deck {
+  const deck: Deck = []
+  for (let i = 1; i <= 13; i++) {
+    deck.push({
+      color: "♠",
+      mark: i
+    })
+    deck.push({
+      color: "♥",
+      mark: i
+    })
+    deck.push({
+      color: "♦",
+      mark: i
+    })
+    deck.push({
+      color: "♣",
+      mark: i
+    })
+  }
+  return deck
+}
+
+function printDeck(deck: Deck) {
+  let result = ""
+  deck.forEach((card, i) => {
+    let str = card.color
+    if (card.mark <= 10) {
+      str += card.mark
+    }
+    else if (card.mark === 11) {
+      str += "J"
+    }
+    else if (card.mark === 12) {
+      str += "Q"
+    }
+    else if (card.mark === 13) {
+      str += "K"
+    }
+    result += str + "\t"
+    if ((i + 1) % 6 === 0) {
+      result += "\n"
+    }
+  })
+  console.log(result)
+}
+
+const deck = createDeck()
+printDeck(deck)
